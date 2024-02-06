8b873bc9e2320cbf03a1cab7e348eff48a3d82db
Roshan Srini <roshansrini26@gmail.com>
2024-02-06 14:24:16 +0530

    Home

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,8 +1,111 @@
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { useToast } from "@/components/ui/use-toast"
+
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {SignInValidation} from "@/lib/validation"
+import { z } from "zod"
+import { Loader } from "lucide-react"
+import { Link, useNavigate } from "react-router-dom"
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
 
 const SigninForm = () => {
+
+  const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+  
+  const { mutateAsync: signInAccount } = useSignInAccount();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignInValidation>>({
+    resolver: zodResolver(SignInValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  
+  
+  async function onSubmit(values: z.infer<typeof SignInValidation>) {
+
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    })
+
+    if(!session) {
+      return toast({ title: 'Sign in failed. Please try again later'})
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn){
+      form.reset();
+
+      navigate('/')
+    } else {
+       return toast({ title: 'Sign-up failed. Please try again'})
+    }
+
+  }
+
   return (
-    <div>SigninForm</div>
+    
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="seiyali-logo"/>
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12"  >Login to your account</h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">Vanakam, please enter your details </p>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" className="shad-input" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">
+            {isUserLoading ? (
+              <div className="flex-center gap-2">
+                 <Loader /> Loading..
+                </div>
+            ):"Sign-in" }
+        </Button>
+        <p className="text-small-regular text-light-2 text-center mt-2">
+              Account illaiya? 
+              <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1" >Sign up</Link>
+        </p>
+      </form>
+      </div>
+    </Form>
   )
 }
 
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,5 +4,10 @@ export const SignUpValidation = z.object({
     name: z.string().min(2, {message: "olunga pera adinga"}),
     username: z.string().min(2, {message: "olunga unga username kudunga"}),
     email: z.string().email({message: "Email id podunga"}),
-    password: z.string().min(8, {message: "thappa podatha"}),
+    password: z.string().min(8, {message: "password 8 keys irukanum"}),
+  })
+
+  export const SignInValidation = z.object({
+    email: z.string().email({message: "Email id podunga"}),
+    password: z.string().min(8, {message: "password 8 keys irukanum"}),
   })
