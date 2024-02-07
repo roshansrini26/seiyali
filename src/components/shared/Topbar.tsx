74a18573728df9639d96071e49b936d78a78e256
Roshan Srini <roshansrini26@gmail.com>
2024-02-07 18:47:00 +0530

    Home/Side

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -2,10 +2,12 @@ import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useEffect } from 'react'
+import { useUserContext } from '@/context/AuthContext'
 
 const Topbar = () => {
     const { mutate: signOut, isSuccess } = useSignOutAccount();
     const navigate = useNavigate();
+    const {user} = useUserContext();
 
     useEffect(()=>{
         if(isSuccess) navigate(0);
@@ -25,10 +27,14 @@ const Topbar = () => {
             </Link>
 
             <div className='flex gap-4'>
-                <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut}>
+                <Button variant="ghost" className='shad-button_ghost' onClick={()=> signOut }>
                     <img src='/assets/icons/logout.svg' alt='logut'/>
                 </Button>
-
+                <Link to={'/profile${user.id}'} className='flex-center gap-3'>
+                    <img src={user.imageUrl || 'assets/images/profile-placeholder.svg'} 
+                    alt='profile' 
+                    className='h-8 w-8 rounded-full' />
+                </Link>
             </div>
 
         </div>
