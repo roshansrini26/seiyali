f4acc8c5eb87c67db218b2eab78398d52befafbf
Roshan Srini <roshansrini26@gmail.com>
2024-02-22 23:23:22 +0530

    like

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -371,7 +371,7 @@ export async function likePost(postId: string, likesArray: string[]) {
 }
 
 // ============================== SAVE POST
-export async function savePost(userId: string, postId: string) {
+export async function savePost(postId: string, userId: string) {
   try {
     const updatedPost = await databases.createDocument(
       appwriteConfig.databaseId,
